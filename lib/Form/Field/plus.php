1fddac2aa5edf24624df6a45a2c45d92329756b1
Romans Malinovskis <r@agiletech.ie>
2012-09-26 15:32:08 +0100

    tweak

--- lib/Form/Field/plus.php
+++ lib/Form/Field/plus.php
@@ -4,15 +4,9 @@ class Form_Field_plus extends Form_Field_basic {
 	function init(){
 		parent::init();
 
-		$v=$this->afterField()->add('ButtonSet');
-		//$v->add('Text')->set('123');
-		$v->add('Button')->set('+');
-		$v->add('Button')->set('-');
-		//$v->addStyle('width','300px');
-		/*->add('Button')->set('+')->js('click',
+		$this->afterField()->add('Button')->set('+')->js('click',
 			$this->js()->autocomplete('search','%')
 			);
-			*/
 	}
 
 }
