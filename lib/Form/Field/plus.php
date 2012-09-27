dc53de8573be62a064e212bbd34a6c24871748f3
Imants Horsts <imants.horsts@inbox.lv>
2012-09-27 17:10:48 +0300

    Update lib/Form/Field/plus.php
    Plus field extension. For buttonset to look good, you need to accept two my pull-requests in atk4 branch.

--- lib/Form/Field/plus.php
+++ lib/Form/Field/plus.php
@@ -3,10 +3,24 @@ namespace autocomplete;
 class Form_Field_plus extends Form_Field_basic {
 	function init(){
 		parent::init();
+		$f = $this->other_field;
 
-		$this->afterField()->add('Button')->set('+')->js('click',
-			$this->js()->autocomplete('search','%')
-			);
+		// Add buttonset to name field
+		$bs = $f->afterField()->add('ButtonSet');
+		
+		// Add buttons
+		$bs->add('Button')
+			->set('+')
+			->js('click',$f->js()->univ()->errorMessage('This should open dialog form for adding new record.'));
+
+		$bs->add('Button') // clear current values
+			->set('-')
+			->js('click',array($f->js()->val(''),$this->js()->val('')));
+		
+		$bs->add('Button') // show current values
+			->set('?')
+			->js('click',$f->js()->univ()->errorMessage('Current values are:<br>ID: $this->js()->val()<br>Name: $f->js()->val()')); // How I can do this???
+		
 	}
 
 }
