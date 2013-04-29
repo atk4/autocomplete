a37d13b29751efedd58b2b4191a1704805413955
Imants Horsts <imants.horsts@inbox.lv>
2013-04-29 15:33:15 +0300

    Tab -> space

--- lib/Form/Field/Plus.php
+++ lib/Form/Field/Plus.php
@@ -3,19 +3,19 @@ namespace autocomplete;
 
 class Form_Field_Plus extends Form_Field_Basic
 {
-	function init()
-	{
-		parent::init();
+    function init()
+    {
+        parent::init();
         $self = $this;
-		
-		$f = $this->other_field;
 
-		// Add buttonset to name field
-		$bs = $f->afterField()->add('ButtonSet');
-		
-		// Add button - open dialog for adding new element
-		$bs->add('Button')
-			->set('+')
+        $f = $this->other_field;
+
+        // Add buttonset to name field
+        $bs = $f->afterField()->add('ButtonSet');
+
+        // Add button - open dialog for adding new element
+        $bs->add('Button')
+            ->set('+')
             ->add('VirtualPage')
             ->bindEvent('Add New Record', 'click')
                 ->set(function($page)use($self) {
@@ -29,5 +29,5 @@ class Form_Field_Plus extends Form_Field_Basic
                         $form->js(null, $js)->univ()->closeDialog()->execute();
                     }
                 });
-	}
+    }
 }
