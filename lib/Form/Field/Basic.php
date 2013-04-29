8b05a5a400e11bb29fe4c1a05c314d06cf341793
Imants Horsts <imants.horsts@inbox.lv>
2013-04-29 15:32:34 +0300

    Tab->space & caption fix

--- lib/Form/Field/Basic.php
+++ lib/Form/Field/Basic.php
@@ -6,132 +6,138 @@ namespace autocomplete;
 
 class Form_Field_Basic extends \Form_Field_Hidden
 {
-	// You can find all available options here: http://jqueryui.com/demos/autocomplete/
-	public $options = array('mustMatch'=>true);
-	
-	// Limits resultset
-	public $limit_rows = 20;
+    // You can find all available options here: http://jqueryui.com/demos/autocomplete/
+    public $options = array('mustMatch'=>true);
 
-	// Minimum characters you have to enter to make autocomplete ajax call
-	public $min_length = 3;
+    // Limits resultset
+    public $limit_rows = 20;
 
-	// Hint text. If empty/null, then hint will not be shown.
-	public $hint = 'Please enter at least %s symbols. Search results will be limited to %s records.';
-	
-	// Text input field object
-	public $other_field;
-	
-	// Model ID field and title field names
-	protected $id_field;
-	protected $title_field;
-	
-	
-	
-	function init()
-	{
-		parent::init();
+    // Minimum characters you have to enter to make autocomplete ajax call
+    public $min_length = 3;
 
-		// add add-on locations to pathfinder
-		$l = $this->api->locate('addons', __NAMESPACE__, 'location');
-		$addon_location = $this->api->locate('addons', __NAMESPACE__);
-		$this->api->pathfinder->addLocation($addon_location, array(
-			'js'  => 'js',
-			'css' => 'templates/css',
-		))->setParent($l);
-		
-		// add additional form field
-		$name = preg_replace('/_id$/', '', $this->short_name);
-		$caption = null;
-		if ($this->owner->model) {
-			if ($f = $this->owner->model->getField($this->short_name)) {
+    // Hint text. If empty/null, then hint will not be shown.
+    public $hint = 'Please enter at least %s symbols. Search results will be limited to %s records.';
+
+    // Text input field object
+    public $other_field;
+
+    // Model ID field and title field names
+    protected $id_field;
+    protected $title_field;
+
+
+
+    function init()
+    {
+        parent::init();
+
+        // add add-on locations to pathfinder
+        $l = $this->api->locate('addons', __NAMESPACE__, 'location');
+        $addon_location = $this->api->locate('addons', __NAMESPACE__);
+        $this->api->pathfinder->addLocation($addon_location, array(
+            'js'  => 'js',
+            'css' => 'templates/css',
+        ))->setParent($l);
+
+        // add additional form field
+        $name = preg_replace('/_id$/', '', $this->short_name);
+        $caption = null;
+        if ($this->owner->model) {
+            if ($f = $this->owner->model->getField($this->short_name)) {
                 $caption = $f->caption();
             }
-		}
-		$this->other_field = $this->owner->addField('line', $name, $caption);
-		if ($this->hint) {
+        }
+        $this->other_field = $this->owner->addField('line', $name, $caption);
+        if ($this->hint) {
             $this->other_field->setFieldHint(sprintf($this->hint, $this->min_length, $this->limit_rows));
         }
-		
-		// move hidden ID field after other field. Otherwise it breaks :first->child CSS in forms
-		$this->js(true)->appendTo($this->other_field->js()->parent());
-		
-		// Set default options
-		if ($this->min_length) {
+
+        // move hidden ID field after other field. Otherwise it breaks :first->child CSS in forms
+        $this->js(true)->appendTo($this->other_field->js()->parent());
+
+        // Set default options
+        if ($this->min_length) {
             $this->options['minLength'] = $this->min_length;
         }
-	}
+    }
 
-	function mustMatch()
-	{
-		$this->options = array_merge($this->options, array('mustMatch'=>'true'));
-		return $this;
-	}
+    function setCaption($_caption)
+    {
+        $this->caption = $this->other_field->caption = $this->api->_($_caption);
+        return $this;
+    }
 
-	function validateNotNULL($msg = null)
-	{
-		$this->other_field->validateNotNULL($msg);
-		return $this;
-	}
+    function mustMatch()
+    {
+        $this->options = array_merge($this->options, array('mustMatch'=>'true'));
+        return $this;
+    }
 
-	function addCondition($q)
-	{
-		$this->model->addCondition($this->title_field, 'like', '%'.$q.'%'); // add condition
-		/*
-		$this->model->addCondition(
-			$this->model->dsql()->orExpr()
-				->where($this->model->getElement( $this->title_field), 'like', '%'.$q.'%')
-				->where($this->model->getElement( $this->id_field), 'like', $this->model->dsql()->getField('id','test'))
-		)->debug();
-		*/
-		$this->model->setOrder($this->title_field); // order ascending by title field
-		if ($this->limit_rows) {
+    function validateNotNULL($msg = null)
+    {
+        $this->other_field->validateNotNULL($msg);
+        return $this;
+    }
+
+    function addCondition($q)
+    {
+        $this->model->addCondition($this->title_field, 'like', '%'.$q.'%'); // add condition
+        /*
+        $this->model->addCondition(
+            $this->model->dsql()->orExpr()
+                ->where($this->model->getElement( $this->title_field), 'like', '%'.$q.'%')
+                ->where($this->model->getElement( $this->id_field), 'like', $this->model->dsql()->getField('id','test'))
+        )->debug();
+        */
+        $this->model->setOrder($this->title_field); // order ascending by title field
+        if ($this->limit_rows) {
             $this->model->_dsql()->limit($this->limit_rows); // limit resultset
         }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	function setOptions($options = array())
-	{
-		$this->options = $options;
-		return $this; //maintain chain
-	}
+    function setOptions($options = array())
+    {
+        $this->options = $options;
+        return $this; //maintain chain
+    }
 
-	function setModel($m, $id_field = null, $title_field = null)
-	{
-		parent::setModel($m);
-		
+    function setModel($m, $id_field = null, $title_field = null)
+    {
+        parent::setModel($m);
+
         $this->id_field = $id_field ?: $this->model->id_field;
         $this->title_field = $title_field ?: $this->model->title_field;
 
-		if ($_GET[$this->name]) {
+        if ($_GET[$this->name]) {
 
-			if ($_GET['term']) {
-				$this->addCondition($_GET['term']);
+            if ($_GET['term']) {
+                $this->addCondition($_GET['term']);
             }
 
-			$data = $this->model->getRows(array($this->id_field, $this->title_field));
+            $data = $this->model->getRows(array($this->id_field, $this->title_field));
 
-			echo json_encode($data);
-			exit;
-		}
-	}
-	
-	function render()
-	{
-		$url = $this->api->url(null, array($this->name => 'ajax'));
-		if ($this->value) { // on add new and inserting allow empty start value
-			$this->model->tryLoad($this->value);
-			$name = $this->model->get($this->title_field);
-			$this->other_field->set($name);
-		}
-		
-		$this->other_field->js(true)
-			->_load('autocomplete_univ')
-			->_css('autocomplete')
-			->univ()
-			->myautocomplete($url, $this, $this->options, $this->id_field, $this->title_field);
+            echo json_encode($data);
+            exit;
+        }
+    }
 
-		return parent::render();
-	}
+    function render()
+    {
+        $url = $this->api->url(null, array($this->name => 'ajax'));
+        if ($this->value) { // on add new and inserting allow empty start value
+            $this->model->tryLoad($this->value);
+            $name = $this->model->get($this->title_field);
+            $this->other_field->set($name);
+        }
+
+        $this->other_field->js(true)
+            ->_load('autocomplete_univ')
+            ->_css('autocomplete')
+            ->univ()
+            ->myautocomplete($url, $this, $this->options, $this->id_field, $this->title_field);
+
+        return parent::render();
+    }
 }
