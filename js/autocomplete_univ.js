7f6c857eb37c9da258b7f312c7ea2c1d73cbe334
Imants Horsts <DarkSide666@users.noreply.github.com>
2016-05-11 15:59:46 +0300

    should trigger hidden field change event manually
    Change event of hidden input fields should be called manually.
    More here: http://stackoverflow.com/a/14834895/1466341

--- js/autocomplete_univ.js
+++ js/autocomplete_univ.js
@@ -10,6 +10,12 @@ $.each({
 
 		this.jquery.autocomplete($.extend({
 			source: data,
+
+			// Triggered when focus is moved to an item (not selecting). The default action
+			// is to replace the text field's value with the value of the focused item, though
+			// only if the event was triggered by a keyboard interaction.
+			// Canceling this event prevents the value from being updated, but does not prevent
+			// the menu item from being focused.
 			focus: function( event, ui ) {
 				// Imants: fix for item selecting with mouse click
 				var e=event;
@@ -18,18 +24,23 @@ $.each({
 
 				return false;
 			},
+			// Triggered when an item is selected from the menu. The default action is to
+			// replace the text field's value with the value of the selected item.
+			// Canceling this event prevents the value from being updated, but does not
+			// prevent the menu from closing.
 			select: function( event, ui ) {
 				q.val( ui.item[title_field] );
-				$(other_field).val( ui.item[id_field] );
-				
+				$(other_field).val( ui.item[id_field] ).trigger('change');
+
 				return false;
 			},
-			change: function(event, ui) {
+			// Triggered when the field is blurred, if the value has changed.
+			change: function( event, ui ) {
 				var data=$.data(this);//Get plugin data for 'this'
 				if(data.uiAutocomplete.selectedItem==undefined) {
 					if("mustMatch" in options) q.val('');
-					$(other_field).val(q.val());
-					
+					$(other_field).val(q.val()).trigger('change');
+
 					return false;
 				}
 			}
